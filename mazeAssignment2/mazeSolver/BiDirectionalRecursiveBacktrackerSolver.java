9e6696e6ed127831f141fdaaed0e07e5ab0a8972
Nyuol <s3243720@student.rmit.edu.au>
2017-10-16 00:52:04 +1100

    please have a look if you can fix it.

--- mazeAssignment2/mazeSolver/BiDirectionalRecursiveBacktrackerSolver.java
+++ mazeAssignment2/mazeSolver/BiDirectionalRecursiveBacktrackerSolver.java
@@ -7,113 +7,11 @@ import maze.Maze;
  */
 public class BiDirectionalRecursiveBacktrackerSolver implements MazeSolver {
 
-    // A class to represent a (row,col) position in
-// a Maze.
-    class Position {
-        // post: return Position to North of this
-        private Position getNorth() {
-            return null;
-        }
-
-        // post: return Position to South of this
-        Position getSouth() {
-            return null;
-        }
-
-        // post: return Position to East of this
-        Position getEast() {
-            return null;
-        }
-
-        // post: return Position to West of this
-        Position getWest() {
-            return null;
-        }
-
-        public boolean equals(Object o) {
-            return false;
-        }
-    }
-
-    // A class to represent a Maze
-    class Maze {
-        Position getStart() {
-            return null;
-        }
-
-        Position getFinish() {
-            return null;
-        }
-
-        // post: return true if p is not a wall
-        boolean isWall() {
-            return isWall();
-        }
-
-        // post: return true if p is not a wall
-     //   boolean isWall(Position p);
-
-        // post: p is marked as visited
-        public void visit() {
-            visit();
-        }
-
-        // post: p is marked as visited
-        public void visit(Position p);
-
-        // post: returns whether p has been visited
-        boolean isVisited(Position p);
-    }
 
 
-    class Solver {
-
-        // post: return a position adjacent to current that is reachable
-        //       and that has not been visited before, or null if not possible.
-        protected Position nextAdjacent(Maze m, Position current) {
-            Position next = current.getNorth();
-            if (!m.isWall(next) && !m.isVisited(next)) {
-                return next;
-            }
-
-            next = current.getEast();
-            if (!m.isWall(next) && !m.isVisited(next)) {
-                return next;
-            }
-
-            next = current.getSouth();
-            if (!m.isWall(next) && !m.isVisited(next)) {
-                return next;
-            }
-
-            next = current.getWest();
-            if (!m.isWall(next) && !m.isVisited(next)) {
-                return next;
-            }
-            return null;
-        }
-
-
-        @Override
+    @Override
     public void solveMaze(Maze maze)  {
-        Stack<Position> path = new StackList<Position>();
 
-        Position current = maze.getStart();
-        maze.visit(current);
-        path.push(current);
-
-        while (!path.empty() && !path.get().equals(maze.getEnd())) {
-            Position next = nextAdjacent(maze, path.get());
-            if (next != null) {
-                maze.visit(next);
-                path.push(next);
-            } else {
-                // No adjacent positions left to try.
-                // Pop position and pick up path on previous.
-                path.pop();
-            }
-        }
-        if (!path.empty()) System.out.println(path);
     }
 
 
--- mazeAssignment2/mazeSolver/WallFollowerSolver.java
+++ mazeAssignment2/mazeSolver/WallFollowerSolver.java
@@ -10,17 +10,52 @@ import maze.*;
 public class WallFollowerSolver implements MazeSolver {
 
 	boolean isSolved = false;
+	int mazeType;
 
 	@Override
 	public void solveMaze(Maze maze) {
 		// TODO Auto-generated method stub
 
+		this.mazeType = mazeType(maze);
+
 		Cell currentCell = maze.entrance;
-		int direction = Maze.SOUTH;
+		int currentDirection = Maze.SOUTH;
 
 		while (!isSolved){
+			maze.drawFtPrt(currentCell);
+			// If the current cell is the exit
+			if(currentCell.equals(maze.exit)) {
+				isSolved = true;
+			}
+			else {
+				// If there is a wall to the left of the cell.
+				int counterClockwiseDirection = getCounterClockwiseDirection(currentDirection);
+				if (currentCell.wall[counterClockwiseDirection].present) {
 
+					// If there is a wall in front
+					if(currentCell.wall[currentDirection].present) {
 
+						// Rotate CW 90 Degrees
+						currentDirection = getClockwiseDirection(currentDirection);
+					}
+					else {
+
+						// Step Forward
+						currentCell = currentCell.neigh[currentDirection];
+					}
+				}
+				// If there is no wall to our left.
+				else {
+
+					// Rotate CCW.
+					currentDirection = getCounterClockwiseDirection(currentDirection);
+
+					// Step forward.
+					currentCell = currentCell.neigh[currentDirection];
+
+				}
+
+			}
 		}
 
 
@@ -41,4 +76,102 @@ public class WallFollowerSolver implements MazeSolver {
 		return 0;
 	} // end of cellsExplored()
 
+
+	private int getCounterClockwiseDirection(int currentDirection) {
+
+		if (mazeType == 0 ) {
+			switch (currentDirection) {
+				case Maze.NORTH:
+					return Maze.WEST;
+				case Maze.WEST:
+					return Maze.SOUTH;
+				case Maze.SOUTH:
+					return Maze.EAST;
+				case Maze.EAST:
+					return Maze.NORTH;
+			}
+		}
+		else if (mazeType == 1) {
+			switch (currentDirection) {
+				case Maze.NORTHWEST:
+					return Maze.WEST;
+				case Maze.WEST:
+					return Maze.SOUTHWEST;
+				case Maze.SOUTHWEST:
+					return Maze.SOUTHEAST;
+				case Maze.SOUTHEAST:
+					return Maze.EAST;
+				case Maze.EAST:
+					return Maze.NORTHEAST;
+				case Maze.NORTHEAST:
+					return Maze.NORTHWEST;
+			}
+		}
+
+
+		return 0;
+	}
+
+
+	private int getClockwiseDirection(int currentDirection) {
+		if (mazeType == 0) {
+			switch (currentDirection) {
+				case Maze.NORTH:
+					return Maze.EAST;
+				case Maze.WEST:
+					return Maze.NORTH;
+				case Maze.SOUTH:
+					return Maze.WEST;
+				case Maze.EAST:
+					return Maze.SOUTH;
+			}
+		}
+		else if (mazeType == 1) {
+			switch (currentDirection) {
+				case Maze.NORTHWEST:
+					return Maze.NORTHEAST;
+				case Maze.WEST:
+					return Maze.NORTHWEST;
+				case Maze.SOUTHWEST:
+					return Maze.WEST;
+				case Maze.SOUTHEAST:
+					return Maze.SOUTHWEST;
+				case Maze.EAST:
+					return Maze.SOUTHEAST;
+				case Maze.NORTHEAST:
+					return Maze.EAST;
+			}
+		}
+
+		return 0;
+	}
+
+	private int mazeType(Maze maze) {
+
+		Cell entrance = maze.entrance;
+		// THe maze is hex if any of these are true.
+		if (entrance.neigh[Maze.NORTHWEST] != null || entrance.neigh[Maze.NORTHEAST] != null
+				|| entrance.neigh[Maze.SOUTHEAST] != null || entrance.neigh[Maze.SOUTHWEST] != null) {
+			System.out.printf("Maze Type Is 1");
+			return 1;
+		}
+		else {
+
+			for (Cell[] cellArray : maze.map) {
+				for (Cell cell : cellArray) {
+					if (cell.tunnelTo != null) {
+						System.out.printf("Maze Type Is 2");
+
+						// Maze Is Tunnel Maze.
+						return 2;
+					}
+				}
+			}
+		}
+
+		// Maze Is Normal Maze
+		System.out.printf("Maze Type Is 0");
+
+		return 0;
+	}
 } // end of class WallFollowerSolver
